70cafd2baca655e27df458246978158adbe558af
noname251 <75530544+noname251@users.noreply.github.com>
2021-12-07 23:03:19 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
 #include<stdio>
 int main(){
+	int multypily(int a,int b);
 	printf("Hellow1 World.");
 	printf("Hellow World.");
 	printf("Hellow Beautiful World.");
 
 	
 	return 0;
+}
+int multypily(int a,int b){
+	return a*b;
 }
